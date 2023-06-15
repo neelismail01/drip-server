3fae2be99d9077dbb47b883dffc58f351bb04edf
Nikhil Ismail <nikhil.ismail20@gmail.com>
2023-06-14 22:51:27 -0700

    generalizing email scraping algo

--- app.py
+++ app.py
@@ -13,32 +13,50 @@ db = client["drip"]
 
 @app.route('/login', methods=["POST"])
 def login():
-    collection = db['users']
+    users_collection = db['users']
+    items_collection = db['items']
     data = request.json
     email = data.get('email')
     name = data.get('name')
 
-    existing_user = collection.find_one({'email': email})
+    # check if user already exists
+    existing_user = users_collection.find_one({'email': email})
     if existing_user:
         return 'User already exists in the collection'
 
+    # insert user into users collection
     user = {
         'email': email,
         'name': name
     }
-    collection.insert_one(user)
+    users_collection.insert_one(user)
+
+    # insert items into items collection
+    user_items = get_items(user["email"])
+    for item in user_items:
+        # if item is already in collection, just append user to users array for that item
+        existing_item = items_collection.find_one({'item_name': item['item_name']})
+        if existing_item:
+            items_collection.update_one({'_id': existing_item['_id']}, {'$addToSet': {'users': user["email"]}})
+        else:
+            item['users'] = [user["email"]]
+            items_collection.insert_one(item)
+
     return 'Data received and saved to users collection'
 
 @app.route('/items', methods=["GET", "POST", "DELETE"])
 def items():
     collection = db['items']
     if request.method == "GET":
-        items = list(collection.find())
+        email = request.args.get('email')
+        query = {"users": email}
+        items = list(collection.find(query))
         json_items = dumps(items)
         return json_items
+    # change to update vs insert
     elif request.method == "POST":
         # Get the data from email scraper
-        data = get_items()
+        data = get_items("")
         result = collection.insert_many(data)
         return "Success", 201
     elif request.method == "DELETE":
