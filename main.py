55c286dafd95a1fd53e5012073c15a426f04d31c
n7ismail <n7ismail@uwaterloo.ca>
2024-06-06 19:06:51 -0400

    deleting unused inbox endpoint

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from flask import (
 )
 from flask_session import Session
 from flask_cors import CORS
-from helpers.email_scraping_service import get_items
 from pymongo import MongoClient
 from bson import ObjectId
 from bson.json_util import dumps
@@ -56,63 +55,6 @@ def similar_items():
     for item in items_list:
             item['_id'] = str(item['_id'])
     return jsonify(items_list), 200
-
-@app.route('/inbox', methods=["GET", "POST", "DELETE"])
-def inbox():
-    users_collection = db['users']
-    items_collection = db['items']
-    brands_collection = db['brands']
-    closet_collection = db['closet']
-
-    if request.method == "POST":
-        data = request.json
-        email = data.get('email')
-        user = users_collection.find_one({'email': email})
-        gender = user['shopping_preference']
-        user_items = get_items(email)
-
-        for item in user_items:
-            # item logic
-            existing_item = items_collection.find_one({'item_name': item['item_name']})
-            if existing_item:
-                if not closet_collection.find_one({'item_id': existing_item['_id'], 'user_id': user['_id']}):
-                    if existing_item['_id'] not in user.get('inbox', []):
-                        users_collection.update_one(
-                            {'_id': user['_id']},
-                            {'$push': {'inbox': existing_item['_id']}}
-                        )
-            else:
-                item['gender'] = gender
-                new_item = items_collection.insert_one(item)
-                item_id = new_item.inserted_id
-                users_collection.update_one(
-                    {'_id': user['_id']},
-                    {'$push': {'inbox': item_id}}
-                )
-            # brand logic
-            existing_brand = brands_collection.find_one({'brand_name': item['brand']})
-            if existing_brand:
-                brands_collection.update_one({'_id': existing_brand['_id']}, {'$inc': {'purchasedCount': 1}})
-            else:
-                brand = {'brand_name': item['brand'], 'purchasedCount': 1}
-                brands_collection.insert_one(brand)
-        return "Successfully added items to the database", 200
-    elif request.method == "GET":
-        email = request.args.get('email')
-        user = users_collection.find_one({'email': email})
-        inbox = user.get('inbox', [])
-        items = items_collection.find({'_id': {'$in': inbox}})
-        items_list = list(items)
-        for item in items_list:
-                item['_id'] = str(item['_id'])
-        return jsonify(items_list), 200
-    elif request.method == "DELETE":
-        email = request.args.get('email')
-        users_collection.update_one(
-            {'email': email},
-            {'$set': {'inbox': []}}
-        )
-        return "Successfully deleted items from inbox", 200
 
 @app.route('/closet', methods=["GET", "POST", "DELETE"])
 def closet():
