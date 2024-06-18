d202fcdbd9348ec03a126a6b7f6ff63111a41ae4
n7ismail <n7ismail@uwaterloo.ca>
2024-06-18 12:07:29 -0400

    adding cohere text embedding api

--- app/services/TextEmbeddingManager.py
+++ app/services/TextEmbeddingManager.py
@@ -1,13 +1,14 @@
-from openai import OpenAI
+from Cohere import cohere
 
 class TextEmbeddingManager():
     def __init__(self, api_key):
-        self.client = OpenAI(api_key=api_key)
+        self.client = cohere.Client(api_key)
 
     def generate_text_embedding(self, text):
-        response = self.client.embeddings.create(
-            input=text,
-            model="text-embedding-3-small"
+        response = self.client.embed(
+            texts=[text],
+            model="embed-english-v3.0",
+            input_type="search_document"
         )
         embedding = response.data[0].embedding
         return embedding
