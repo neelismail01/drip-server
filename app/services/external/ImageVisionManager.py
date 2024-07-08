8db8894c65b35123bad9d7d5d648e8eefe20b85e
n7ismail <n7ismail@uwaterloo.ca>
2024-07-08 16:15:40 -0400

    adding custom prompt for outfit descriptions

--- app/services/external/ImageVisionManager.py
+++ app/services/external/ImageVisionManager.py
@@ -1,4 +1,5 @@
 from openai import OpenAI
+from app.utils.constants import ITEM_IMAGE_VISION_PROMPT, OUTFIT_IMAGE_VISION_PROMPT
 
 class ImageVisionManager:
     def __init__(self, api_key):
@@ -11,23 +12,24 @@ class ImageVisionManager:
                 {
                     "role": "user",
                     "content": [
-                        {
-                            "type": "text",
-                            "text": (
-                                """
-                                Your task is to describe an image of a provided fashion item.
-                                Please classify the type of fashion item and provide details
-                                about the color and fabric. Additionally, mention some occasions
-                                where this item could be worn. Do not suggest other fashion items 
-                                that would go well with this item. Please keep your entire 
-                                description under 50 words.
-                                """
-                            )
-                        },
-                        {
-                            "type": "image_url",
-                            "image_url": { "url": f"data:image/jpeg;base64,{base64_image}" },
-                        },
+                        { "type": "text", "text": ITEM_IMAGE_VISION_PROMPT },
+                        { "type": "image_url", "image_url": { "url": f"data:image/jpeg;base64,{base64_image}" } },
+                    ],
+                }
+            ],
+            max_tokens=300,
+        )
+        return response.choices[0].message.content
+
+    def get_details_from_outfit_image(self, base64_image):
+        response = self.client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        { "type": "text", "text": OUTFIT_IMAGE_VISION_PROMPT },
+                        { "type": "image_url", "image_url": { "url": f"data:image/jpeg;base64,{base64_image}" } },
                     ],
                 }
             ],
